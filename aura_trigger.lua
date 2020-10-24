ee6302f5a3ea72a7b81f42c5900dc633bf6f7f38
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2020-10-24 13:26:53 +0300

    Added null check in cases tonumber is unable to parse npc_id

--- aura_trigger.lua
+++ aura_trigger.lua
@@ -5,6 +5,16 @@ function(allstates, event, unit)
     if (event == 'UNIT_AURA' and unit ~= 'target') then
         return false
     end
+
+    if (not UnitExists('target') or not UnitIsEnemy("player", "target") or UnitIsDead('target') or UnitIsPlayer('target')) then
+        -- In case we don't have target or target is dead or target is player or friend we just
+        -- remove all auras
+        for _, state in pairs(allstates) do
+            state.show = false
+            state.changed = true
+        end
+        return true
+    end
     
     local target_GUID = UnitGUID('target')
     if not target_GUID then
@@ -12,16 +22,17 @@ function(allstates, event, unit)
     end
     
     local type, zero, server_id, instance_id, zone_uid, npc_id, spawn_uid = strsplit("-", target_GUID);
-
     if not npc_id then
-        -- Some GUID can't be parsed
         return false
     end
     
     npc_id = tonumber(npc_id)
+    if not npc_id then
+        return false
+    end
     
     local effects = aura_env.mobs[npc_id]
-    if not effects or not UnitExists('target') or UnitIsPlayer('target') then 
+    if not effects then 
         -- Reset all states and disable all effects
         for _, state in pairs(allstates) do
             state.show = false
